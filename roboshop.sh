1d67e2656a6ac8f965908faaf8e88696215f9a5d
Vishwaknani <mailtokrish94@gmail.com>
2024-01-23 16:14:38 +0530

    10th commit

--- roboshop.sh
+++ roboshop.sh
@@ -20,10 +20,11 @@ do
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0e8c45124526c632d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
 
+    
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
+    --hosted-zone-id 1234567890ABC \
     --change-batch '
-    {
+   {
     "Comment": "Testing creating a record set"
     ,"Changes": [{
       "Action"              : "CREATE"
@@ -32,7 +33,7 @@ do
         ,"Type"             : "A"
         ,"TTL"              : 1
         ,"ResourceRecords"  : [{
-            "Value"         : "'$IP_ADDRESS'"
+            "Value"         : ""
         }]
       }
     }]
