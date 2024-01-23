9c48df6505897ee3746efd5be100eba280adf338
Vishwaknani <mailtokrish94@gmail.com>
2024-01-23 15:05:33 +0530

    10th commit

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,9 @@
 AMI=ami-0f3c7d07486cad139
 SG_ID=sg-0e8c45124526c632d
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
- 
+ZONE_ID=Z0824185329GEFCO1CV59
+DOMAIN_NAME="vishwak.online"
+
 for i in "${INSTANCES[@]}"
 do
     echo "instance is: $i"
@@ -12,8 +14,32 @@ do
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
-    fi        
+    fi       
+
+    echo "$i: $IP_ADDRESS" 
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0e8c45124526c632d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID\
+    --change-batch '
+    {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+         "Name"              : $i "'.$DOMAIN_NAME"
+         ,"Type"             : "A"
+         ,"TTL"              : 1
+         ,"ResourceRecords"  : [{
+             "Value"         : "$IP_ADDRESS"
+         }]
+        }
+     }]
+    }
+    "
+
+    
+
 done
 
