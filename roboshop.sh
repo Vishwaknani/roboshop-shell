f1c7003bf9e9acea4f12ad9971b9b9915f2aebdb
Vishwaknani <mailtokrish94@gmail.com>
2024-01-23 18:06:00 +0530

    10th commit

--- roboshop.sh
+++ roboshop.sh
@@ -8,36 +8,34 @@ DOMAIN_NAME="Vishwak.online"
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-    then 
-        INSTANCE_TYPE="t3.small"
+    if [[ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]]; then
+    INSTANCE_TYPE="t2.small"
     else
         INSTANCE_TYPE="t2.micro"
-    fi       
+    fi
 
-    echo "$i: $IP_ADDRESS" 
-
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0e8c45124526c632d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
-
-    
+    PRIVATE_IPADDRESS=$(aws ec2 run-instances \
+                    --image-id ami-03265a0778a880afb \
+                    --count 1 \
+                    --instance-type "$INSTANCE_TYPE" \
+                    --security-group-ids sg-02deaf6b5f3dd44c0 \
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
+                    --query 'Instances[0].PrivateIpAddress' \
+                    --output text)
+    echo "$i : $PRIVATE_IPADDRESS" 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-    "Comment": "Testing creating a record set"
-    ,"Changes": [{
-      "Action"              : "CREATE"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$i'.'$DOMAIN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP_ADDRESS'"
-         }]
-        }
-      }]
-     }
-    '
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch "{
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$i.$DOMAIN_NAME\",
+                \"Type\": \"A\",
+                \"TTL\": 1,
+                \"ResourceRecords\": [{\"Value\": \"$PRIVATE_IPADDRESS\"}]
+            }
+        }]
+    }"
+    echo "record created successfully for $i : $i.$DOMAIN_NAME"
 done
 
