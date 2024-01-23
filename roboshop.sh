87cc5db4b5261d7b494207ef1c8937e278306ab9
Vishwaknani <mailtokrish94@gmail.com>
2024-01-23 12:12:16 +0530

    10th commit

--- roboshop.sh
+++ roboshop.sh
@@ -2,20 +2,18 @@
 
 AMI=ami-0f3c7d07486cad139
 SG_ID=sg-0e8c45124526c632d
-INSTANCES=(")
-INSTANCES=("mongodb" "redis" "mysql" rabbitmq" "catalogue" "user" "cart" "shipping"
-"payment" "dispatch" "web")
+INSTANCES=(""mongodb" "redis" "mysql" rabbitmq" "catalogue" "user" "cart" "shipping""payment" "dispatch" "web")
 
 for i in "${INSTANCES[@]}"
 do
-   if [$i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-   then 
-       INSTANCE_TYPE="t3.small"
-   else
-       INSTANCE_TYPE="t2.micro"
-   fi    
+    echo "instance is: $i"
+    if [$i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then 
+        INSTANCE_type="t3.small"
+    else
+        INSTANCE_type="t2.micro"
+    fi        
 
-   aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro
-   --security-group-ids sg-0e8c45124526c632d
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-0e8c45124526c632d
 done
 
