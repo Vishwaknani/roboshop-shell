7a1daabcfdcc4a518ee04b00e3aeb3aec7fae863
Vishwaknani <mailtokrish94@gmail.com>
2024-01-23 12:49:00 +0530

    10th commit

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
     echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then 
-        INSTANCE_type="t3.small"
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_type="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi        
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-0e8c45124526c632d --tag-specifications "ResourceType=instance, Tags=[{Key=name,Value=$i}]"
