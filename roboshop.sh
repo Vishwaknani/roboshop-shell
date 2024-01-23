0c81f8a64117e400f8000a68e4e2ab054819b8cf
Vishwaknani <mailtokrish94@gmail.com>
2024-01-23 15:47:55 +0530

    10th commit

--- roboshop.sh
+++ roboshop.sh
@@ -28,11 +28,11 @@ do
           ,"Changes": [{
            "Action"              : "CREATE"
            ,"ResourceRecordSet"  : {
-           "Name"              : "'$i'.'$DOMAIN_NAME'"
-           ,"Type"             : "A"
-           ,"TTL"              : 1
-           ,"ResourceRecords"  : [{ 
-             "Value"           : '$IP_ADDRESS'
+             "Name"              : "'$i'.'$DOMAIN_NAME'"
+             ,"Type"             : "A"
+             ,"TTL"              : 1
+             ,"ResourceRecords"  : [{ 
+               "Value"           : "'$IP_ADDRESS'"
          }]
         }
       }]
